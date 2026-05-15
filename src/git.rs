b18dba193c6bf2e1b5959d3906db4c5b4dadfa3d
angelou6 <elpepechingon@outlook.com>
2026-05-15 15:15:55 -0600

    accept multiple -m

--- src/git.rs
+++ src/git.rs
@@ -26,68 +26,73 @@ pub fn unstage(location: &str) {
     run_command(&["git", "restore", "--staged", location]);
 }
 
+fn git_messages(messages: &[String]) -> Vec<&str> {
+    let mut res: Vec<&str> = Vec::new();
+
+    for message in messages {
+        res.push("-m");
+        res.push(message);
+    }
+
+    res
+}
+
 pub fn add_and_commit(message: Vec<String>, force: bool, all: bool) {
     if get_staged().len() == 0 || all {
         run_command(&["git", "add", "."]);
     }
 
-    match message.len() {
-        0 => {
-            if force {
-                let files = get_staged();
-                let count = files.len();
-                let msg = format!(
-                    "Changed {} file{}",
-                    count,
-                    if count == 1 { "" } else { "s" }
-                );
-                let list_str = format!("Files changed: {}", files.join(", "));
-                run_command(&["git", "commit", "-m", &msg, "-m", &list_str]);
-            }
-        }
-        1 => run_command(&["git", "commit", "-m", &message[0]]),
-        2 => run_command(&["git", "commit", "-m", &message[0], "-m", &message[1]]),
-        _ => unreachable!(),
+    if message.len() == 0 && force {
+        let files = get_staged();
+        let count = files.len();
+        let msg = format!(
+            "Changed {} file{}",
+            count,
+            if count == 1 { "" } else { "s" }
+        );
+        let list_str = format!("Files changed: {}", files.join(", "));
+        run_command(&["git", "commit", "-m", &msg, "-m", &list_str]);
+    } else {
+        let mut args = vec!["git", "commit"];
+        let mut messages = git_messages(&message);
+        args.append(&mut messages);
+        run_command(&args);
     }
 }
 
 pub fn amend_commit(message: Vec<String>) {
     run_command(&["git", "add", "."]);
 
-    match message.len() {
-        0 => run_command(&["git", "commit", "--amend", "--no-edit"]),
-        1 => run_command(&["git", "commit", "--amend", "-m", &message[0]]),
-        2 => run_command(&[
-            "git",
-            "commit",
-            "--amend",
-            "-m",
-            &message[0],
-            "-m",
-            &message[1],
-        ]),
-        _ => unreachable!(),
+    if message.len() == 0 {
+        run_command(&["git", "commit", "--amend", "--no-edit"]);
+    } else {
+        let mut args = vec!["git", "commit"];
+        let mut messages = git_messages(&message);
+        args.append(&mut messages);
+        run_command(&args);
     }
 }
 
 pub fn push(message: Vec<String>, force: bool, all: bool) {
     add_and_commit(message, force, all);
-
-    if force {
-        run_command(&["git", "push", "--force"]);
+    run_command(if force {
+        &["git", "push", "--force"]
     } else {
-        run_command(&["git", "push"]);
-    }
+        &["git", "push"]
+    });
 }
 
 pub fn amend_push(message: Vec<String>, force: bool) {
     amend_commit(message);
-    let force = if force {
-        "--force"
-    } else {
-        "--force-with-lease"
-    };
-    run_command(&["git", "push", force]);
+    run_command(&[
+        "git",
+        "push",
+        if force {
+            "--force"
+        } else {
+            "--force-with-lease"
+        },
+    ]);
 }
 
 pub fn pull() {
@@ -123,16 +128,16 @@ pub fn force_pull(skip: bool) {
     run_command(&["git", "reset", "--hard", "@{u}"]);
 }
 
-pub fn undo_commit(hard: bool) {
+pub fn undo_commit(hard: bool, commit: String) {
     if hard {
-        run_command(&["git", "reset", "--hard", "HEAD~1"]);
+        run_command(&["git", "reset", "--hard", &commit]);
     } else {
-        run_command(&["git", "reset", "HEAD~1"]);
+        run_command(&["git", "reset", &commit]);
     }
 }
 
-pub fn undo_push(force: bool, hard: bool) {
-    undo_commit(hard);
+pub fn undo_push(force: bool, hard: bool, commit: String) {
+    undo_commit(hard, commit);
     run_command(&[
         "git",
         "push",
